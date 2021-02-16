9046a4c9d3fa231e94db8d0b9efdf35926263c03
Artem <artemisuzl@gmail.com>
2021-02-16 21:48:35 +0300

    Создал нужные фрагменты. Добавил сохранение состояния.

--- app/src/main/java/com/uzlov/myapplication/ListNotesFragment.java
+++ app/src/main/java/com/uzlov/myapplication/ListNotesFragment.java
@@ -1,5 +1,7 @@
 package com.uzlov.myapplication;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,16 +13,23 @@ import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class ListNotesFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     private ListView lvNotes;
-    private static final String ARG_INDEX = "index";
-    private int index;
+    public static final String CURRENT_NOTE = "CurrentNote";
+    private Note currentNote;
+    public static final String ARG_INDEX = "index";
+    private boolean isLandscape;
+    private List<Note> notes = new ArrayList<>();
 
     public static ListNotesFragment newInstance(int index) {
         ListNotesFragment f = new ListNotesFragment();    // создание
-
         // Передача параметра
         Bundle args = new Bundle();
         args.putInt(ARG_INDEX, index);
@@ -31,9 +40,11 @@ public class ListNotesFragment extends Fragment implements AdapterView.OnItemCli
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            index = getArguments().getInt(ARG_INDEX);
-        }
+        notes.add(new Note("Заметка 1", "нет ничего", new Date().toString(), "Artem"));
+        notes.add(new Note("Заметка 2", "что то", new Date().toString(), "Artem"));
+        notes.add(new Note("Заметка 3", "пусто", new Date().toString(), "Artem"));
+        notes.add(new Note("Продукты", "молоко и хлеб", new Date().toString(), "Artem"));
+        notes.add(new Note("Заметка 5"));
     }
 
     @Override
@@ -49,20 +60,75 @@ public class ListNotesFragment extends Fragment implements AdapterView.OnItemCli
         initView(view);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+
+        if (savedInstanceState != null){
+            currentNote = savedInstanceState.getParcelable(CURRENT_NOTE);
+        } else {
+            currentNote = new Note("Первая");
+        }
+
+        if (isLandscape){
+            showLandNote(currentNote);
+        }
+    }
+
+    private void showNotes(Note note) {
+        if (isLandscape) {
+            showLandNote(note);
+        } else {
+            showPortNote(note);
+        }
+    }
+
+    private void showLandNote(Note note) {
+        NoteFragment fragment = NoteFragment.newInstance(note);
+
+        if (getActivity() != null){
+            getActivity().getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.note_desc_container, fragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            .commit();
+        }
+    }
+
+    private void showPortNote(Note note) {
+        Intent intent = new Intent(getContext(), DetailActivity.class);
+        // и передадим туда параметры
+        intent.putExtra(ARG_INDEX, note);
+        startActivity(intent);
+    }
+
     private void initView(View view){
         lvNotes = view.findViewById(R.id.listViewNotes);
         lvNotes.setOnItemClickListener(this);
-        final String[] stringArray = getResources().getStringArray(R.array.notes);
+
+        final String[] titles = new String[]{"Заметка 1",
+                "Заметка 2",
+                "Заметка 3",
+                "Продукты",
+                "Заметка 5"};
 
         // используем адаптер данных
         ArrayAdapter<String> adapter = new ArrayAdapter<>(view.getContext(),
-                android.R.layout.simple_list_item_1, stringArray);
+                android.R.layout.simple_list_item_1, titles);
 
         lvNotes.setAdapter(adapter);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        currentNote = notes.get(position);
+        showNotes(currentNote);
+    }
 
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putParcelable(CURRENT_NOTE, currentNote);
+        super.onSaveInstanceState(outState);
     }
 }
--- app/src/main/java/com/uzlov/myapplication/NoteFragment.java
+++ app/src/main/java/com/uzlov/myapplication/NoteFragment.java
@@ -8,15 +8,39 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
 public class NoteFragment extends Fragment {
 
+    private TextView tvDate;
     private TextView tvName;
     private TextView tvAuthor;
     private EditText etDescription;
 
+    private static final String ARG_INDEX = "index";
+    private Note index;
+
+    public static NoteFragment newInstance(Note note) {
+        NoteFragment f = new NoteFragment();    // создание
+
+        // Передача параметра
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_INDEX, note);
+        f.setArguments(args);
+        return f;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            index = getArguments().getParcelable(ARG_INDEX);
+        } else {
+            index = new Note("Первая");
+        }
+    }
 
     @Override
     public View onCreateView(
@@ -29,11 +53,20 @@ public class NoteFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initView(view);
+        fillView();
     }
 
     private void initView(View view){
-        tvName = view.findViewById(R.id.textview_name);
+        tvDate = view.findViewById(R.id.textview_date);
+        tvName = view.findViewById(R.id.textViewName);
         tvAuthor = view.findViewById(R.id.textView_author);
         etDescription = view.findViewById(R.id.etDescription);
+    }
+
+    private void fillView() {
+        etDescription.setText(index.getDescription());
+        tvAuthor.setText(index.getAuthor());
+        tvDate.setText(index.getDateCreate());
+        tvName.setText(index.getName());
     }
 }
