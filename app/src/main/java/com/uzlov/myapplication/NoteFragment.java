9acd5360a8e60c5ff66f0faa8d67abfdd29cdfff
Artem <artemisuzl@gmail.com>
2021-02-16 22:11:17 +0300

    Обновил строки. Добавил возможность менять время заметки

--- app/src/main/java/com/uzlov/myapplication/NoteFragment.java
+++ app/src/main/java/com/uzlov/myapplication/NoteFragment.java
@@ -1,9 +1,13 @@
 package com.uzlov.myapplication;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,13 +15,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
 
-public class NoteFragment extends Fragment {
+
+
+public class NoteFragment extends Fragment{
 
     private TextView tvDate;
     private TextView tvName;
     private TextView tvAuthor;
     private EditText etDescription;
+
+    private OnChangeDateListener onChangeDateListener = new OnChangeDateListener() {
+        @Override
+        public void onChangeDate(String newDate) {
+            tvDate.setText(newDate);
+        }
+    };
 
     private static final String ARG_INDEX = "index";
     private Note index;
@@ -61,6 +76,23 @@ public class NoteFragment extends Fragment {
         tvName = view.findViewById(R.id.textViewName);
         tvAuthor = view.findViewById(R.id.textView_author);
         etDescription = view.findViewById(R.id.etDescription);
+
+        tvDate.setOnClickListener(v -> {
+            if (getActivity() != null){
+
+                MaterialTimePicker picker =
+                        new MaterialTimePicker.Builder()
+                                .setTimeFormat(TimeFormat.CLOCK_24H)
+                                .setTitleText(R.string.select_new_string)
+                                .build();
+
+                picker.show(getActivity().getSupportFragmentManager(), "datePicker");
+
+                picker.addOnPositiveButtonClickListener(v1 -> {
+                    onChangeDateListener.onChangeDate(picker.getHour()+":" + picker.getMinute());
+                });
+            }
+        });
     }
 
     private void fillView() {
@@ -69,4 +101,30 @@ public class NoteFragment extends Fragment {
         tvDate.setText(index.getDateCreate());
         tvName.setText(index.getName());
     }
+
+
+//    public static class DatePickerFragment extends DialogFragment
+//            implements DatePickerDialog.OnDateSetListener {
+//
+//        private OnChangeDateListener onChangeDateListener;
+//
+//        public DatePickerFragment(OnChangeDateListener onChangeDateListener) {
+//            super();
+//            this.onChangeDateListener = onChangeDateListener;
+//        }
+//
+//        @Override
+//        public Dialog onCreateDialog(Bundle savedInstanceState) {
+//            final Calendar c = Calendar.getInstance();
+//            int year = c.get(Calendar.YEAR);
+//            int month = c.get(Calendar.MONTH);
+//            int day = c.get(Calendar.DAY_OF_MONTH);
+//
+//            return new DatePickerDialog(getActivity(), this, year, month, day);
+//        }
+//
+//        public void onDateSet(DatePicker view, int year, int month, int day) {
+//            onChangeDateListener.onChangeDate(day + "." + (month+1) + "." + year);
+//        }
+//    }
 }
