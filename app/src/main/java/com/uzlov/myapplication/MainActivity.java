0a0828d4de1799f23bd7225b79e0f574f8f73c4f
Artem <artemisuzl@gmail.com>
2021-02-19 18:41:20 +0300

    Добавил боковое меню + исправил небольшие ошибки и неточности.

--- app/src/main/java/com/uzlov/myapplication/MainActivity.java
+++ app/src/main/java/com/uzlov/myapplication/MainActivity.java
@@ -3,22 +3,35 @@ package com.uzlov.myapplication;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements OnChangeCurrentNote {
     private boolean isLandscape;
     private Note currentNote;
     public static final String ARG_INDEX = "index";
     private boolean isFistLaunch;
+    private Toolbar toolbar;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_drawer);
+
+        initView();
+
         isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
 
         if (savedInstanceState != null && savedInstanceState.getParcelable(ARG_INDEX) != null){
@@ -35,6 +48,59 @@ public class MainActivity extends AppCompatActivity implements OnChangeCurrentNo
         } else {
             showPortNote(currentNote);
         }
+    }
+
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // STUB!
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.settings_item:
+                // STUB!
+                return true;
+            case R.id.about_item:
+                // STUB!
+                return true;
+            case R.id.popular_item:
+                // STUB!
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.drawer_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     private void showLandNote(Note currentNote) {
@@ -55,7 +121,6 @@ public class MainActivity extends AppCompatActivity implements OnChangeCurrentNo
     }
 
     private void showPortNote(Note currentNote) {
-        Log.e("showPortNote", "showPortNote");
         getSupportFragmentManager()
             .beginTransaction()
             .replace(R.id.container_fragments, ListNotesFragment.newInstance(this, currentNote))
@@ -64,7 +129,6 @@ public class MainActivity extends AppCompatActivity implements OnChangeCurrentNo
     }
 
     private void showPortNewNote(Note currentNote) {
-        Log.e("showPortNote", "showPortNote");
         getSupportFragmentManager()
             .beginTransaction()
             .replace(R.id.container_fragments, NoteFragment.newInstance(currentNote))
@@ -74,6 +138,7 @@ public class MainActivity extends AppCompatActivity implements OnChangeCurrentNo
 
     @Override
     public void onBackPressed() {
+        setTitleToolbar(getString(R.string.app_name));
         if (!isLandscape){
             getSupportFragmentManager()
                 .beginTransaction()
@@ -98,9 +163,16 @@ public class MainActivity extends AppCompatActivity implements OnChangeCurrentNo
         Log.e("MainActivity, restore", currentNote.getName());
     }
 
+    private void setTitleToolbar(String title){
+        toolbar.setTitle(title);
+    }
+
     @Override
     public void newCurrentNote(Note note) {
         currentNote = note;
+
+        setTitleToolbar(note.getName());
+
         if (isLandscape){
             showLandNote(currentNote);
         } else {
